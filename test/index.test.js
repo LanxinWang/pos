e0d10ef6e7a911302966659ca85c6140c3d5f506
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-12-08 21:46:29 +0800

    fix dexcribe about tests

--- test/index.test.js
+++ test/index.test.js
@@ -11,15 +11,15 @@ const mockLoadAllItems = [
   },
   {
     barcode: "ITEM000001",
-    name: "雪碧",
-    unit: "瓶",
-    price: 3.0,
-  },
-  {
-    barcode: "ITEM000002",
     name: "苹果",
     unit: "斤",
     price: 5.5,
+  },
+  {
+    barcode: "ITEM000002",
+    name: "雪碧",
+    unit: "瓶",
+    price: 3.0,
   },
   {
     barcode: "ITEM000003",
@@ -31,7 +31,7 @@ const mockLoadAllItems = [
 const mockLoadPromotions = [
   {
     type: "BUY_TWO_GET_ONE_FREE",
-    barcodes: ["ITEM000000", "ITEM000001"],
+    barcodes: ["ITEM000002", "ITEM000003"],
   },
 ];
 
@@ -59,8 +59,23 @@ describe("pos print receipts when all items bought with no BUY_TWO_FREE_ONE prom
     expect(result).toEqual(expectReceipt);
   });
 
-  it("should print correct receipt when buy 3 of the same item with no BUY_TWO_FREE_ONE promotions", () => {
-    const inputTags = ["ITEM000000"];
+  it("should print correct receipt when buy 1-pound weigh-in item with no BUY_TWO_FREE_ONE promotions", () => {
+    const inputTags = ["ITEM000001-1"];
+    const expectReceipt = `
+    ***<没钱赚商店>收据***
+    名称：苹果，数量：1斤，单价：5.50(元)，小计：5.50(元)
+    ----------------------
+    总计：5.50(元)
+    节省：0.00(元)
+    **********************`;
+
+    const result = printReceipt(inputTags);
+
+    expect(result).toEqual(expectReceipt);
+  });
+
+  it("should print correct receipt when buy 3 same items with no BUY_TWO_FREE_ONE promotions", () => {
+    const inputTags = ["ITEM000000", "ITEM000000", "ITEM000000"];
     const expectReceipt = `
     ***<没钱赚商店>收据***
     名称：可口可乐，数量：3瓶，单价：2.50(元)，小计：7.50(元)
@@ -74,14 +89,13 @@ describe("pos print receipts when all items bought with no BUY_TWO_FREE_ONE prom
     expect(result).toEqual(expectReceipt);
   });
 
-  it("should print correct receipt when buy 1 each of 2 different items with no BUY_TWO_FREE_ONE promotions", () => {
-    const inputTags = ["ITEM000000", "ITEM000001"];
+  it("should print correct receipt when buy 3-pounds same weigh-in items with no BUY_TWO_FREE_ONE promotions", () => {
+    const inputTags = ["ITEM000001-3"];
     const expectReceipt = `
     ***<没钱赚商店>收据***
-    名称：可口可乐，数量：1瓶，单价：2.50(元)，小计：2.50(元)
-    名称：雪碧，数量：1瓶，单价：3.00(元)，小计：3.00(元)
+    名称：苹果，数量：3斤，单价：5.50(元)，小计：16.50(元)
     ----------------------
-    总计：5.50(元)
+    总计：16.50(元)
     节省：0.00(元)
     **********************`;
 
@@ -90,14 +104,14 @@ describe("pos print receipts when all items bought with no BUY_TWO_FREE_ONE prom
     expect(result).toEqual(expectReceipt);
   });
 
-  it("should print correct receipt when buy 3 each of 2 different items with no BUY_TWO_FREE_ONE promotions", () => {
-    const inputTags = ["ITEM000000"];
+  it("should print correct receipt when buy 1 item and 2-pound item with no BUY_TWO_FREE_ONE promotions", () => {
+    const inputTags = ["ITEM000000", "ITEM000001-2"];
     const expectReceipt = `
     ***<没钱赚商店>收据***
-    名称：可口可乐，数量：3瓶，单价：2.50(元)，小计：7.50(元)
-    名称：雪碧，数量：3瓶，单价：3.00(元)，小计：9.00(元)
+    名称：可口可乐，数量：1瓶，单价：2.50(元)，小计：2.50(元)
+    名称：苹果，数量：2斤，单价：5.50(元)，小计：11.00(元)
     ----------------------
-    总计：16.50(元)
+    总计：13.50(元)
     节省：0.00(元)
     **********************`;
 
