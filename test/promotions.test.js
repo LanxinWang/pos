2b1dac206858083e74da0b6670370627fb5a9fe3
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-12-08 15:13:32 +0800

    add a get promotions test

---
+++ test/promotions.test.js
@@ -0,0 +1,14 @@
+const loadPromotions = require("../main/promotions");
+
+describe("promotions", () => {
+  it("should get promotions information when call loadPromotions function", () => {
+    const promotionsInformation = [
+      {
+        type: "BUY_TWO_GET_ONE_FREE",
+        barcodes: ["ITEM000000", "ITEM000001", "ITEM000005"],
+      },
+    ];
+    const result = loadPromotions();
+    expect(result).toEqual(promotionsInformation);
+  });
+});
