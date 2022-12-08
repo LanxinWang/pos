1476712f7b0e8b82de8ab3e39348a92f2b8faab6
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-12-08 15:16:06 +0800

    implemente loadPromotions function

---
+++ main/promotions.js
@@ -0,0 +1,10 @@
+const promotionsInformation = [
+  {
+    type: "BUY_TWO_GET_ONE_FREE",
+    barcodes: ["ITEM000000", "ITEM000001", "ITEM000005"],
+  },
+];
+
+const loadPromotions = () => promotionsInformation;
+
+module.exports = loadPromotions;
