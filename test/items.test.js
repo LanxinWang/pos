91f13ac1eab434e431e5c36f15b453b4d501dcab
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-12-08 14:43:13 +0800

    add a get items test

---
+++ test/items.test.js
@@ -0,0 +1,44 @@
+describe("items", () => {
+  it("should get all items' information when call loadAllItems function", () => {
+    const itemsInformation = [
+      {
+        barcode: "ITEM000000",
+        name: "可口可乐",
+        unit: "瓶",
+        price: 3.0,
+      },
+      {
+        barcode: "ITEM000001",
+        name: "雪碧",
+        unit: "瓶",
+        price: 3.0,
+      },
+      {
+        barcode: "ITEM000002",
+        name: "苹果",
+        unit: "斤",
+        price: 5.5,
+      },
+      {
+        barcode: "ITEM000003",
+        name: "荔枝",
+        unit: "斤",
+        price: 15.0,
+      },
+      {
+        barcode: "ITEM000004",
+        name: "电池",
+        unit: "个",
+        price: 2.0,
+      },
+      {
+        barcode: "ITEM000005",
+        name: "方便面",
+        unit: "袋",
+        price: 4.5,
+      },
+    ];
+    const result = loadAllItems();
+    expect(result).toEqual(itemsInformation);
+  });
+});
