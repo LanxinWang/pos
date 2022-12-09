0db9b7196a300490aa5727fdb78ef96b3de649f6
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-12-09 17:06:24 +0800

    implemente printReceipt function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-// import _ from "lodash";
+import _ from "lodash";
 
-const _ = require("lodash");
-const { loadAllItems } = require("./items");
-const allItems = loadAllItems();
-
-const printReceipt = (inputTags, promotions) => {
-  console.log(allItems);
-
-  const purchasedItems = getItemsBy(inputTags, promotions);
+export const printReceipt = (inputTags, allItems, promotions) => {
+  const purchasedItems = getItemsBy(inputTags, allItems, promotions);
   const totalPrice = calculateTotalPriceFor(purchasedItems);
   const totalCount = calculateTotalCountFor(purchasedItems);
   const printPurchasedItemsDetails = printPurchasedItemsFormat(purchasedItems);
@@ -20,45 +14,48 @@ const printReceipt = (inputTags, promotions) => {
     **********************`;
 };
 
-const getItemsBy = (inputTags, promotions) => {
-  let result = [];
-  //generate purchased items
-  for (let tag = 0; tag < inputTags.length; tag++) {
-    const itemBarcode = inputTags[tag].split("-")[0];
-    const number = inputTags[tag].split("-")[1] || 1;
-    console.log("number:", number);
-    const item = _.find(allItems, (item) => item.barcode === itemBarcode);
-    console.log(
-      "``````:",
-      _.find(result, (item) => item.barcode === itemBarcode)
+const getItemsBy = (inputTags, allItems, promotions) => {
+  let purchasedItems = [];
+  for (let i = 0; i < inputTags.length; i++) {
+    const purchasedItemBarcode = inputTags[i].split("-")[0];
+    const number = inputTags[i].split("-")[1] || 1;
+    const item = _.find(
+      allItems,
+      (item) => item.barcode === purchasedItemBarcode
     );
-    // if (_.find(result, (item) => item.barcode === itemBarcode)) {
-    //   console.log("-----enter-----");
-    //   const index = _.findIndex(
-    //     result,
-    //     (boughtItem) => boughtItem.barcode === itemBarcode
-    //   );
-    //   console.log("index", index);
-    //   console.log("result[index]", result[index]);
-    //   result[index].num += number;
-    // }
+
+    const index = _.findIndex(
+      purchasedItems,
+      (purchasedItem) => purchasedItem.barcode === purchasedItemBarcode
+    );
+
+    if (index >= 0) {
+      purchasedItems[index].num += number;
+      continue;
+    }
+
     const promotionType = _.find(promotions, (promotion) =>
-      promotion.barcodes.includes(itemBarcode)
+      promotion.barcodes.includes(purchasedItemBarcode)
     )?.type;
-    const subtotal = calculateAKindOfItemSubtotalBy(
-      promotionType,
-      item.price,
-      number
-    );
-    result.push({
+
+    purchasedItems.push({
+      barcode: item.barcode,
       name: item.name,
       unitPrice: item.price,
       unit: item.unit,
       num: number,
       promotionType,
-      subtotal,
+      subtotal: 0,
     });
   }
+  const result = purchasedItems.map((item) => {
+    item.subtotal = calculateAKindOfItemSubtotalBy(
+      item.promotionType,
+      item.unitPrice,
+      item.num
+    );
+    return item;
+  });
   return result;
 };
 
@@ -73,7 +70,7 @@ const calculateTotalCountFor = (purchasedItems) =>
   purchasedItems.reduce(
     (totalCount, item) =>
       totalCount +
-      calculateAItemCountBy(item.promotionType, item.price, item.num),
+      calculateAItemCountBy(item.promotionType, item.unitPrice, item.num),
     0
   );
 
@@ -105,15 +102,14 @@ const calculateAItemCountBy = (itemPromotionType, itemUnitPrice, num) => {
 
 const printPurchasedItemsFormat = (purchasedItems) =>
   purchasedItems
-    .map((boughtItem) => {
-      return `名称：${boughtItem.name}，数量：${boughtItem.num}${
-        boughtItem.unit
-      }，单价：${boughtItem.price.toFixed(
-        2
-      )}(元)，小计：${boughtItem.subtotal.toFixed(2)}(元)`;
-    })
-    .join("\n");
-
-module.exports = {
-  printReceipt,
-};
+    .map(
+      (boughtItem) =>
+        `名称：${boughtItem.name}，数量：${boughtItem.num}${
+          boughtItem.unit
+        }，单价：${boughtItem.unitPrice.toFixed(
+          2
+        )}(元)，小计：${boughtItem.subtotal.toFixed(2)}(元)
+    `
+    )
+    .join("")
+    .trim();
