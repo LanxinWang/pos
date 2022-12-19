cbdf1a46ff06507ecc404fcf286a6f04bfd5956a
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-12-19 11:40:07 +0800

    test:input a barcode and get a cart item

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,5 @@
-const { getCartBarcodes } = require("../src/index");
+const { getCartBarcodes, buildCartItems } = require("../src/index");
+const { loadAllItems } = require("../src/fixtures.js");
 
 describe("getCartBarcodes", () => {
   it("should return cart barcodes when input non-weighting tags", () => {
@@ -9,7 +10,7 @@ describe("getCartBarcodes", () => {
 
     expect(result).toEqual(expectCartBarcodes);
   });
-  it("should return cart barcodes when input weighting tags", function () {
+  it("should return cart barcodes when input weighting tags", () => {
     const tag = ["ITEM000002-1", "ITEM000003-2"];
     const expectCartBarcodes = ["ITEM000002", "ITEM000003", "ITEM000003"];
 
@@ -17,7 +18,7 @@ describe("getCartBarcodes", () => {
 
     expect(result).toEqual(expectCartBarcodes);
   });
-  it("should return cart barcodes when input all kinds of tags", function () {
+  it("should return cart barcodes when input all kinds of tags", () => {
     const tag = ["ITEM000000", "ITEM000003-2"];
     const expectCartBarcodes = ["ITEM000000", "ITEM000003", "ITEM000003"];
 
@@ -25,4 +26,59 @@ describe("getCartBarcodes", () => {
 
     expect(result).toEqual(expectCartBarcodes);
   });
+});
+
+describe("buildCartItems", () => {
+  it("should return right build cart items when input a cartBarcode", () => {
+    const allItems = loadAllItems();
+    const cartBarcodes = ["ITEM000000"];
+    const expectCartItems = [
+      {
+        barcode: "ITEM000000",
+        name: "可口可乐",
+        unit: "瓶",
+        price: 3.0,
+        count: 1,
+      },
+    ];
+
+    const result = buildCartItems(cartBarcodes, allItems);
+
+    expect(result).toEqual(expectCartItems);
+  });
+  // it("should return right build cart items when input cartBarcodes", function () {
+  //   const allItems = loadAllItems();
+  //   const cartBarcodes = [
+  //     "ITEM000000",
+  //     "ITEM000000",
+  //     "ITEM000001",
+  //     "ITEM000002",
+  //     "ITEM000003",
+  //     "ITEM000003",
+  //     "ITEM000003",
+  //     "ITEM000003",
+  //   ];
+  //   const expectCartItems = [
+  //     {
+  //       barcode: "ITEM000000",
+  //       name: "可口可乐",
+  //       unit: "瓶",
+  //       price: 3.0,
+  //       count: 2,
+  //     },
+  //     { barcode: "ITEM000001", name: "雪碧", unit: "瓶", price: 3.0, count: 1 },
+  //     { barcode: "ITEM000002", name: "苹果", unit: "斤", price: 5.5, count: 1 },
+  //     {
+  //       barcode: "ITEM000003",
+  //       name: "荔枝",
+  //       unit: "斤",
+  //       price: 15.0,
+  //       count: 4,
+  //     },
+  //   ];
+  //
+  //   const result = buildCartItems(cartBarcodes, allItems);
+  //
+  //   expect(result).toEqual(expectCartItems);
+  // });
 });
