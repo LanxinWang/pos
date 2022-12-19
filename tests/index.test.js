54a0e569568a869f7b4070f26655c2168d46989d
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-12-19 13:20:43 +0800

    test:input BUY_TWO_FREE_ONE cart items and right receipt items

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,9 @@
-const { getCartBarcodes, buildCartItems } = require("../src/index");
-const { loadAllItems } = require("../src/fixtures.js");
+const {
+  getCartBarcodes,
+  buildCartItems,
+  buildReceiptItems,
+} = require("../src/index");
+const { loadAllItems, loadPromotions } = require("../src/fixtures.js");
 
 describe("getCartBarcodes", () => {
   it("should return cart barcodes when input non-weighting tags", () => {
@@ -81,4 +85,160 @@ describe("buildCartItems", () => {
 
     expect(result).toEqual(expectCartItems);
   });
+});
+
+describe("buildReceiptItems", () => {
+  it("should return right receipt items when input no BUY_TWO_FREE_ONE cart items", function () {
+    const allPromotions = loadPromotions();
+    const cartItems = [
+      { barcode: "ITEM000001", name: "雪碧", unit: "瓶", price: 3.0, count: 1 },
+      {
+        barcode: "ITEM000003",
+        name: "荔枝",
+        unit: "斤",
+        price: 15.0,
+        count: 4,
+      },
+    ];
+    const expectReceiptItems = [
+      {
+        cartItem: {
+          barcode: "ITEM000001",
+          name: "雪碧",
+          unit: "瓶",
+          price: 3.0,
+          count: 1,
+        },
+        discount: 0.0,
+        subtotal: 3.0,
+      },
+      {
+        cartItem: {
+          barcode: "ITEM000003",
+          name: "荔枝",
+          unit: "斤",
+          price: 15.0,
+          count: 4,
+        },
+        discount: 0.0,
+        subtotal: 60.0,
+      },
+    ];
+
+    const result = buildReceiptItems(cartItems, allPromotions);
+
+    expect(result).toEqual(expectReceiptItems);
+  });
+  it("should return right receipt items when input BUY_TWO_FREE_ONE cart items", function () {
+    const allPromotions = loadPromotions();
+    const cartItems = [
+      {
+        barcode: "ITEM000000",
+        name: "可口可乐",
+        unit: "瓶",
+        price: 3.0,
+        count: 3,
+      },
+      { barcode: "ITEM000002", name: "苹果", unit: "斤", price: 5.5, count: 7 },
+    ];
+    const expectReceiptItems = [
+      {
+        cartItem: {
+          barcode: "ITEM000000",
+          name: "可口可乐",
+          unit: "瓶",
+          price: 3.0,
+          count: 3,
+        },
+        discount: 3.0,
+        subtotal: 6,
+      },
+      {
+        cartItem: {
+          barcode: "ITEM000002",
+          name: "苹果",
+          unit: "斤",
+          price: 5.5,
+          count: 7,
+        },
+        discount: 11.0,
+        subtotal: 27.5,
+      },
+    ];
+
+    const result = buildReceiptItems(cartItems, allPromotions);
+
+    expect(result).toEqual(expectReceiptItems);
+  });
+  // it("should return right receipt items when input all kinds of cart items", function () {
+  //   const allPromotions = loadPromotions();
+  //   const cartItems = [
+  //     {
+  //       barcode: "ITEM000000",
+  //       name: "可口可乐",
+  //       unit: "瓶",
+  //       price: 3.0,
+  //       count: 3,
+  //     },
+  //     { barcode: "ITEM000001", name: "雪碧", unit: "瓶", price: 3.0, count: 1 },
+  //     { barcode: "ITEM000002", name: "苹果", unit: "斤", price: 5.5, count: 7 },
+  //     {
+  //       barcode: "ITEM000003",
+  //       name: "荔枝",
+  //       unit: "斤",
+  //       price: 15.0,
+  //       count: 4,
+  //     },
+  //   ];
+  //   const expectReceiptItems = [
+  //     {
+  //       cartItem: {
+  //         barcode: "ITEM000000",
+  //         name: "可口可乐",
+  //         unit: "瓶",
+  //         price: 3.0,
+  //         count: 3,
+  //       },
+  //       discount: 3.0,
+  //       subtotal: 6,
+  //     },
+  //     {
+  //       cartItem: {
+  //         barcode: "ITEM000001",
+  //         name: "雪碧",
+  //         unit: "瓶",
+  //         price: 3.0,
+  //         count: 1,
+  //       },
+  //       discount: 0.0,
+  //       subtotal: 3.0,
+  //     },
+  //     {
+  //       cartItem: {
+  //         barcode: "ITEM000002",
+  //         name: "苹果",
+  //         unit: "斤",
+  //         price: 5.5,
+  //         count: 7,
+  //       },
+  //       discount: 11.0,
+  //       subtotal: 27.5,
+  //     },
+  //     {
+  //       cartItem: {
+  //         barcode: "ITEM000003",
+  //         name: "荔枝",
+  //         unit: "斤",
+  //         price: 15.0,
+  //         count: 4,
+  //       },
+  //       discount: 0.0,
+  //       subtotal: 60.0,
+  //     },
+  //   ];
+  //
+  //   const result = buildReceiptItems(cartItems, allPromotions);
+  //
+  //   expect(result).toEqual(expectReceiptItems);
+  // });
 });
